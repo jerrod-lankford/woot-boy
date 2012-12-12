01b4af2ba900d6d59c11012d93452cec824d9685
Jerrod Lankford <Jllankfo@ncsu.edu>
2012-12-12 00:29:19 -0500

    Disable menus on startup and check woot off the list on startup

--- src/WootIcon.py
+++ src/WootIcon.py
@@ -27,7 +27,7 @@ class WootIcon(wx.TaskBarIcon):
 
     def CreateMenu(self):
         checkMenu = wx.Menu()
-        checkMenu.AppendCheckItem(201,"Woot!")
+        checkMenu.AppendCheckItem(201,"Woot!").Check(True)
         checkMenu.AppendCheckItem(202,"Home")
         checkMenu.AppendCheckItem(203,"Sport")
         checkMenu.AppendCheckItem(204,"Kids")
@@ -35,7 +35,7 @@ class WootIcon(wx.TaskBarIcon):
         
         menu = wx.Menu()
         menu.Append(100,"&Start","Start")
-        menu.Append(101,"S&top","Stop")
+        menu.Append(101,"S&top","Stop").Enable(False)
         menu.AppendMenu(200,"Woots",checkMenu)
         menu.Append(102,"&Close","Close")
         EVT_MENU(self,100,self.frame.PopupAction)
